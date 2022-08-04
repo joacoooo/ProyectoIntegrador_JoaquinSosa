e38c1045217d028af4f728aee6e6c128ee84fd27
joacoooo <c_sardoy@hotmail.com>
2022-08-04 15:18:42 -0300

    Componentes

--- ProyectoIntegrador_JoaquinSosa/src/app/app.module.ts
+++ ProyectoIntegrador_JoaquinSosa/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgPrComponent } from './components/logo-arg-pr/logo-arg-pr.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgPrComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
